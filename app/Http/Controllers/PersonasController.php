eddf2e6c4b1ea9c59476cfa5c67d4837f4117ecd
Andres E. Salazar A <P025773864@activo.com>
2023-12-07 16:35:09 -0400

    se agrego controllador y  modelo para personas, agregado a eso se creo base de datos y se comento los controladores que se van a utilizar

---
+++ app/Http/Controllers/PersonasController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Personas;
+use Illuminate\Http\Request;
+
+class PersonasController extends Controller
+{
+
+    public function index()
+    {
+        //pagina de inicio
+    }
+
+
+    public function create()
+    {
+        //el formulario donde nosotros agregamos datos
+    }
+
+
+    public function store(Request $request)
+    {
+        //sirve para guardar datos en la DB
+    }
+
+    public function show(Personas $personas)
+    {
+        //servidor para obtener un registro de nuestra tabla
+    }
+
+ 
+    public function edit(Personas $personas)
+    {
+        //este metodo nos sirve para traer los datos que se van a editar y los coloca en un formulario
+
+    }
+
+
+    public function update(Request $request, Personas $personas)
+    {
+        //este metodo actualiza los datos en la BD
+    }
+
+    public function destroy(Personas $personas)
+    {
+        //elimina un registro
+    }
+}
