856adaf45d11fd6c2d444689fede46cf0e9ba6f2
Andres E. Salazar A <P025773864@activo.com>
2023-12-28 10:19:44 -0400

    se realizo modificacion de texto en formulario para modificar datos de usuario

--- resources/views/user_edit.blade.php
+++ resources/views/user_edit.blade.php
@@ -13,19 +13,24 @@
             <form action="{{ route('user.update', $users->id) }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
-                <label for="paterno">Nombre de usuario</label>
-                <input type="text" name="name" class="form-control"  value="{{ $users->name }}">
+                <label for="paterno">Modifica tu nombre</label>
+                <input type="text" name="name" class="form-control" value="{{ $users->name }}">
 
-                <label for="materno">Correo de usuario</label>
-                <input type="text" name="email" class="form-control"  value="{{ $users->email }}">
+                <label for="materno">Modifica tu correo</label>
+                <input type="text" name="email" class="form-control" value="{{ $users->email }}">
 
-                <label for="formFile" class="form-label">Avatar de usuario</label>
+                <label for="formFile" class="form-label">Cambia tu avatar de usuario</label>
+                <br>
+                <img src="http://localhost/laravelCrud/public/{{ $users->avatar }}" alt="{{ $users->avatar }}">
+                <br>
                 <input class="form-control" type="file" id="formFile" name="avatar">
+
                 <br>
 
                 <a href="{{ route('user.index') }}" class="btn btn-info">
                     <span class="fas fa-undo-alt"></span> Regresar al perfil
                 </a>
+
                 <button class="btn btn-primary"><span class="fas fa-user-edit"></span> Actualizar</button>
             </form>
             </p>
